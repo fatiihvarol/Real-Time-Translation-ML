34ec008cf5a0e0d7a804526080eb30fca68840af
fatih varol <fatihvrl38@gmail.com>
2024-05-20 23:20:23 +0300

    first commit

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
 from flask import Flask, request, jsonify, render_template
-
 from googletrans import Translator
 
 app = Flask(__name__)
 
-def translate_text(text, target_language='en'):
-    """
-    Metin çevirisi yapar.
-    
-    :param text: Çevrilecek metin.
-    :param target_language: Hedef dil (varsayılan olarak İngilizce).
-    :return: Çevrilmiş metin.
-    """
-    translator = Translator()
-    translated_text = translator.translate(text, dest=target_language)
-    return translated_text.text
+translator = Translator()
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/translate', methods=['POST'])
-def translate():
-    input_text = request.form['input_text']
-    translator = Translator()
-    translated_text = translator.translate(input_text, dest='en')
-    return jsonify({'translated_text': translated_text.text })
+def translate_text():
+    input_text = request.form.get('input_text', '')
+    if input_text:
+        translated_text = translator.translate(input_text).text
+    else:
+        translated_text = "No input provided."
+    return jsonify({'translated_text': translated_text})
 
 if __name__ == "__main__":
     app.run(debug=True)
